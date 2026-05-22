80f8b9ec4f9dc18f5232c7ee8a78972a7f47860c
NightSwordCoder <2817305907@qq.com>
2026-05-22 16:32:35 +0800

    新增MapSelection

--- assets/Script/SceneLoader.ts
+++ assets/Script/SceneLoader.ts
@@ -4,9 +4,12 @@ const { ccclass, property } = _decorator;
 @ccclass('SceneLoader')
 export class SceneLoader extends Component {
 
-    private SceneList: string[] = ["Map0", "Map1", "Menu"];
+    private SceneList: string[] = ["Map0", "Map1", "Menu","MapSelection"];
     public LoadScene1(){
         director.loadScene(this.SceneList[1]);
+    }
+    public LoadSceneMap(){
+        director.loadScene(this.SceneList[3]);
     }
 }
 
